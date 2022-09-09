c75214a23cc192832eb662480d45363f1e5fcf97
Jeroen Lammersma <jeroen@lammersma.dev>
2022-09-10 00:29:50 +0200

    try

--- setup/dev-env/dev_env_setup.sh
+++ setup/dev-env/dev_env_setup.sh
@@ -7,10 +7,6 @@ function create_file() {
 }
 
 function setup_dev_environment() {
-  # if ! command -v "pipenv" > /dev/null 2>&1; then
-  #   echo "Installing pipenv..."
-  #   sudo apt-get install pipenv
-  # fi
   sudo apt-get install pipenv -y
 
 
@@ -63,8 +59,11 @@ function setup_dev_environment() {
 function setup_pipenv() {
   # ensure pip is working correctly by re-installing it
   echo "Configuring pip..."
-  curl -s https://bootstrap.pypa.io/get-pip.py -o "$scratch/get-pip.py"
-  cd "$ROOT" && pipenv run python3 "$scratch/get-pip.py" > /dev/null
+  # curl -s https://bootstrap.pypa.io/get-pip.py -o "$scratch/get-pip.py"
+  # cd "$ROOT" && pipenv run python3 "$scratch/get-pip.py" > /dev/null
+
+  pipenv run "pip install --upgrade pip"
+  pipenv run "pip install --upgrade setuptools"
 
   echo "Installing pip packages..."
   pipenv install --dev
