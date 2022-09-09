adc9dff3cb390a3a94187be81aad872925fa796c
Jeroen Lammersma <jeroen@lammersma.dev>
2022-09-09 21:55:31 +0200

    try fix

--- setup/openpilot/openpilot_setup.sh
+++ setup/openpilot/openpilot_setup.sh
@@ -33,7 +33,7 @@ if [ -n "$OPENPILOT_ENV" ] || [ -d "$HOME/.pyenv" ]; then
 fi
 cd "$OPENPILOT_PATH"
 if ! tools/ubuntu_setup.sh; then
-  exec bash
+  . ~/.bashrc
   cd "$OPENPILOT_PATH"
   tools/ubuntu_setup.sh
 fi
