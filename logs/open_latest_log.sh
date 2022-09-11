5eb54906ad004891bcc7a5cc787e4c8f53e2aa83
Jeroen Lammersma <jeroen@lammersma.dev>
2022-09-11 13:21:17 +0200

    Fix open_latest_log script, now opening latest log

--- logs/open_latest_log.sh
+++ logs/open_latest_log.sh
@@ -13,15 +13,31 @@ validate_openpilot_logs_path
 
 re="^[[:digit:]]{4}-"  # first 4 chars need to be digits, followed by a hyphen
 
-latest_path=$(cut -d " " -f 1 <<< $(echo $OPENPILOT_LOGS_PATH/*/))
-latest_path=$(readlink -m "$latest_path")
-latest_basename=$(basename "$latest_path")
+# create array and populate with log paths
+logs=()
+for path in "$OPENPILOT_LOGS_PATH"/*/; do
+  path=$(readlink -m "$path")
+  # only append log paths, skip other directories
+  if [[ $(basename "$path") =~ $re ]]; then
+    logs+=( "$path" )
+  fi
+done
 
-if [[ $latest_basename =~ $re ]]; then
-  segment=${1:-0}
-  export PIPENV_PIPFILE="$OPENPILOT_PATH/Pipfile"
-  pipenv run python "$OPENPILOT_PATH"/tools/plotjuggler/juggle.py "${latest_path%"--"*}--$segment/rlog"
-else
-  echo "No logs found." >&2
+# abort if array is empty
+if [ -z "${logs[*]}" ]; then
+  echo "No logs found, directory is empty. Abort." >&2
   exit 1
 fi
+
+# sort logs, descending order
+IFS=$'\n' sorted_logs=($(sort -r <<<"${logs[*]}")); unset IFS
+
+# get latest log
+latest_log=${sorted_logs[0]}
+
+# set segment (default is 0)
+segment=${1:-0}
+
+# open log with plotjuggler, trunc segment from path (--#) and concat $segment
+export PIPENV_PIPFILE="$OPENPILOT_PATH/Pipfile"
+pipenv run python "$OPENPILOT_PATH"/tools/plotjuggler/juggle.py "${latest_log%"--"*}--$segment/rlog"
